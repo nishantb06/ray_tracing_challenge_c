973fa61c5e6ee4e5f6006dcf241e83fb8e24f357
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-04-03 12:26:57 +0530

    feat: final plot_sphere

--- ray_tracer/plot_sphere.c
+++ ray_tracer/plot_sphere.c
@@ -10,24 +10,19 @@
 
 int main()
 {
-    Canvas c = Canvas_(100, 100);
+    Canvas c = Canvas_(1000, 1000);
     Color red = Color_(1, 0, 0);
     Sphere s = Sphere_(1, 1);
     s.material = DefaultMaterial();
     s.material->color = Color_(1, 0.2, 1);
-    // Apply a shearing and scaling transformation to the sphere
-    // Matrix *shear = Shearing(1, 0, 0, 0, 0, 0);
-    // Matrix *scale = Scaling(0.5, 1, 1);
-    // Matrix *transform = MultiplyMatrices(shear, scale);
-    // SetTransform(&s, transform);
 
     Tuple light_position = Point(-10, 10, -10);
     Color light_color = Color_(1, 1, 1);
     Light *light = Light_(light_position, light_color);
 
-    // Tuple ray_origin = Point(0, 0, -5);
+    Tuple ray_origin = Point(0, 0, -5); // Fixed ray origin
     float wall_z = 10;
-    float wall_size = 10;
+    float wall_size = 7; // Adjusted for better framing
     float pixel_size = wall_size / c.width;
     float half = wall_size / 2;
 
@@ -38,19 +33,17 @@ int main()
         {
             float world_x = -half + pixel_size * x;
             Tuple position = Point(world_x, world_y, wall_z);
-            Tuple direction = Subtract(position, light_position);
+            Tuple direction = Subtract(position, ray_origin); // From ray origin to position
             Normalize(&direction);
-            Ray r = Ray_(light_position, direction);
+            Ray r = Ray_(ray_origin, direction);
             Intersections *intersections = Intersect(s, r);
             if (Hit(intersections) != NULL)
-            {   
-                printf("Hit\n");
+            {
                 Tuple point = Position(r, Hit(intersections)->t);
-                Tuple *normal = NormalAt(s, point);
+                Tuple* normal = NormalAt(s, point);                                    // Get the normal at the hit point
                 Tuple eye = r.direction;
-                ScalerMultiply(&eye, -1);
-                Color color = Lighting(s.material, light, point, eye, *normal);
-                PrintColor(&color);
+                Negate(&eye);                                      // Eye vector points back to the ray origin
+                Color color = Lighting(s.material, light, point, eye, *normal); // False for inShadow
                 WritePixel(&c, x, y, color);
             }
         }
