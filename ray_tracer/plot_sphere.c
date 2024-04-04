3e51427b68dd24616b5ea11f797bc6875ad286c4
nbhansali06@gmail.com <nbhansali06@gmail.com>
2024-04-04 19:28:48 +0530

    fix: cleanup

--- ray_tracer/plot_sphere.c
+++ ray_tracer/plot_sphere.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include "tuple.h"
-#include "matrix.h"
-#include "transformation.h"
-#include "ray.h"
-#include "canvas.h"
+#include "src/tuple.h"
+#include "src/matrix.h"
+#include "src/transformation.h"
+#include "src/ray.h"
+#include "src/canvas.h"
 
 int main()
 {
-    Canvas c = Canvas_(1000, 1000);
+    Canvas c = Canvas_(500, 500);
     Color red = Color_(1, 0, 0);
     Sphere s = Sphere_(1, 1);
     s.material = DefaultMaterial();
-    s.material->color = Color_(1, 0.2, 1);
+    s.material->color = Color_(1, 0.2, 0.2);
     // Apply a shearing and scaling transformation to the sphere
     Matrix *shear = Shearing(1, 0, 0, 0, 0, 0);
     Matrix *scale = Scaling(0.5, 1, 1);
     Matrix *transform = MultiplyMatrices(shear, scale);
     SetTransform(&s, transform);
 
-    Tuple light_position = Point(-10, 10, -10);
+    Tuple light_position = Point(-5, 5, -10);
     Color light_color = Color_(1, 1, 1);
     Light *light = Light_(light_position, light_color);
 
